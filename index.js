07f6fbe695053a04f322adb0fc49abe92c2c7447
MartinC02 <martincarlsen9@gmail.com>
2023-04-06 21:18:12 +0200

    update

--- index.js
+++
@@ -1,6 +0,0 @@
-const ratings = document.querySelectorAll("button:hover");
-var rating;
-stars.forEach((rating, idx) => {
-    rating = idx + 1;
-        consolore.log(`rating ${rating} was clicked`);
-});
